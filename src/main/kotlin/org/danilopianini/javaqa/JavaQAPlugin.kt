3dc06a7f124849c359311af0f89d7be6c1195cbe
Danilo Pianini <danilo.pianini@unibo.it>
2021-11-05 11:28:12 +0100

    fix: use the proper extension for the SpotBugs exclusions file

--- src/main/kotlin/org/danilopianini/javaqa/JavaQAPlugin.kt
+++ src/main/kotlin/org/danilopianini/javaqa/JavaQAPlugin.kt
@@ -45,7 +45,7 @@ open class JavaQAPlugin : Plugin<Project> {
                     setEffort("max")
                     setReportLevel("low")
                     showProgress.set(false)
-                    val excludes = createFromResource(spotbugsExcludes, File(javaQADestination, "spotbugs-excludes"))
+                    val excludes = createFromResource(spotbugsExcludes, File(javaQADestination, "spotbugs-excludes.xml"))
                     excludeFilter.set(excludes)
                 }
                 // Checkstyle
