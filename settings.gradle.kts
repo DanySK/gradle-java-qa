356c63ab6b3de78fbb7cfb39f0333065cc7655fe
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-17 14:54:53 +0200

    build: migrate to Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,17 @@
 rootProject.name = "gradle-java-qa"
 
 plugins {
-    id("com.gradle.enterprise") version "3.17.5"
+    id("com.gradle.develocity") version "3.17.5"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.8"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+    }
 }
 
 gitHooks {
@@ -12,12 +20,4 @@ gitHooks {
     }
     commitMsg { conventionalCommits() }
     createHooks()
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishOnFailure()
-    }
 }
