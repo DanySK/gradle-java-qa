a2de391abc8856b8a3c41b598c6dd569a3061f77
Danilo Pianini <danilo.pianini@unibo.it>
2024-12-09 13:04:36 +0100

    ci(release): disambiguate the release task name

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,6 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
-./gradlew uploadKotlinOSSRHToMavenCentralNexus uploadPluginMavenToMavenCentralNexus uploadPluginMarkerMavenToMavenCentralNexus release || exit 1
+./gradlew uploadKotlinOSSRHToMavenCentralNexus uploadPluginMavenToMavenCentralNexus uploadPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2  
 git push --force origin \${nextRelease.version} || exit 3
 ./gradlew publishJavaOSSRHPublicationToGithubRepository || true
