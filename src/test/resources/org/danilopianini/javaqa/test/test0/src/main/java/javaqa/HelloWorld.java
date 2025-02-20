8c57b109ff9b6afd0d454de90ec569e69d1fd2c7
Danilo Pianini <danilo.pianini@unibo.it>
2025-02-20 13:46:26 +0100

    test: add check on setter method

--- src/test/resources/org/danilopianini/javaqa/test/test0/src/main/java/javaqa/HelloWorld.java
+++ src/test/resources/org/danilopianini/javaqa/test/test0/src/main/java/javaqa/HelloWorld.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 
 final class HelloWorld {
 
+    private NetworkManager networkManager = new NetworkManager();
+
     private HelloWorld() {
+    }
+
+    private void setNetworkManager(final NetworkManager networkManager) {
+        this.networkManager = networkManager;
     }
 
     public static void main(final String... a) {
@@ -15,9 +21,15 @@ final class HelloWorld {
             .filter(it -> it % 2 != 0)
             .map(it -> it + 1)
             .forEach(System.out::println);
+        final HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setNetworkManager(new NetworkManager());
+        System.out.println(helloWorld.networkManager); // NOPMD
         System.out.println("Hello, World -> " + anotherVar); // NOPMD
         System.out.println("[ " + System.out + " " + System.class + " " + Runtime.class + " " + anotherVar + " ]"); // NOPMD
         System.out.println("[ " + System.out + "-" + System.class + "-" + Runtime.class + "-" + anotherVar + " ]"); // NOPMD
         System.out.println(Arrays.toString(new int[] {1, 2, 3, 8, 16, 32, 100})); // NOPMD
+    }
+
+    private static final class NetworkManager {
     }
 }
