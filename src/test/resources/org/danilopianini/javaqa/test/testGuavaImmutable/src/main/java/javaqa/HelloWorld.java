eedd65fce5c7e65095510923e212b150326916f3
Danilo Pianini <danilo.pianini@gmail.com>
2026-04-19 12:08:51 +0200

    chore(core-deps): update PMD to v7.23.0

--- src/test/resources/org/danilopianini/javaqa/test/testGuavaImmutable/src/main/java/javaqa/HelloWorld.java
+++ src/test/resources/org/danilopianini/javaqa/test/testGuavaImmutable/src/main/java/javaqa/HelloWorld.java
@@ -2,13 +2,21 @@ package javaqa;
 
 import com.google.common.collect.ImmutableMap;
 
-final class HelloWorld {
+/**
+ * Simple entry point used by integration tests.
+ */
+public final class HelloWorld {
 
     private static final ImmutableMap<String, String> MAP = ImmutableMap.of("key", "value");
 
     private HelloWorld() {
     }
 
+    /**
+     * Launches the sample application.
+     *
+     * @param a ignored command line arguments
+     */
     public static void main(final String... a) {
         System.out.println("Hello, World " + MAP); // NOPMD
     }
